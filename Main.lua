fa2ac27889a0f608367f63125dbdc43f1fd2749d
plusmouse <plusmouse@protonmail.com>
2022-09-05 13:42:07 +0100

    Add unit price printing

--- Main.lua
+++ Main.lua
@@ -1,13 +1,26 @@
-local prev = SortAuctionSetSort
+local originalSortAuctionSetSort = SortAuctionSetSort
 hooksecurefunc("SortAuctionSetSort", function()
-  prev("list", "unitprice")
+  originalSortAuctionSetSort("list", "unitprice")
+end)
+
+hooksecurefunc("MoneyFrame_Update", function(frameName, money)
+  local buyoutMoneyFrameName = "BrowseButton1BuyoutFrameMoney"
+  if frameName == buyoutMoneyFrameName or _G[buyoutMoneyFrameName] == frameName  then
+    local count = 1
+    if BrowseButton1ItemCount:IsShown() then
+      count = tonumber(BrowseButton1ItemCount:GetText())
+    end
+    print("unit price", GetMoneyString(math.floor(money / count)))
+  end
 end)
 
 
 AuctionHouseFixesMixin = {}
+
 function AuctionHouseFixesMixin:OnLoad()
   self:RegisterEvent("AUCTION_HOUSE_SHOW")
 end
+
 function AuctionHouseFixesMixin:OnEvent()
   self:UnregisterEvent("AUCTION_HOUSE_SHOW")
   BrowseBuyoutButton:HookScript("OnClick", function()
